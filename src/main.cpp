e03817ba7a7d2a4fd1e75526ca308054c8e041e8
kk161205 <kulshrestha.kushagrak1512@gmail.com>
2026-02-03 08:36:57 +0530

    Initializing the pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #define irPin 2
 void setup() {
-
+    pinMode(irPin, INPUT);
 }
 
 void loop() {
-
+    int irValue = digitalRead(irPin);
+    if (irValue == HIGH) {
+        Serial.println("IR SENSOR: NO Obstacles ");
+    }else {
+        Serial.println("IR SENSOR: Obstacles detected");
+    }
+    delay(500);
 }
